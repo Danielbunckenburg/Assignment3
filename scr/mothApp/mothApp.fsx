34eb4f77840404eca7dd043cee34bf2cd2cf8eb6
Danie <daniel_bunckenburg@hotmail.dk>
2024-12-18 11:48:15 +0100

    Version1.0.3

--- scr/mothApp/mothApp.fsx
+++ scr/mothApp/mothApp.fsx
@@ -1,5 +1,7 @@
 #r "nuget:DIKU.Canvas"
 #load "asteroids.fs"
+
+
 open Canvas
 open Color
 open System
@@ -8,106 +10,103 @@ open Asteroids.Asteroids
 open Asteroids.Vectors
 open Asteroids.RandomGenerator
 
-
-
-//Canvas Size
-let width, height = 800, 600
-
-//The initial state of the light and the state change
+// The initial state of the light
 let initialState = false
 
-//Auxiliary functions for Moth-class
-//let lightPosition = (float width / 2.0, (float height / 3.0) + 40.0)
+// Coordinates function to wrap around edges
+let coordinates (koordinates: float) (maxValue: float) =
+    if koordinates < 0.0 then maxValue + koordinates
+    elif koordinates > maxValue then koordinates - maxValue
+    else koordinates
 
-let donutCoordinate (coord: float) (maxVal: float) =
-    if coord < 0.0 then maxVal + coord
-    elif coord > maxVal then coord - maxVal
-    else coord
+// Size of the window
+let w, h = 800, 600
 
-//Moth-class
-type Moth (pos: Vec, hdng: float) =
-    let mutable _hdng = hdng
-    let mutable _pos = pos
-    new () = 
-        let randomX = float (rnd.Next(0, width))
-        let randomY = float (rnd.Next(0, height))
+// Light position
+let lightPos = (float w / 2.0, (float h / 3.0) + 40.0)
+
+// Moth type definition
+type Moth(pos: Vec, hdng: float) =
+    let mutable pos2 = pos
+    let mutable hdng2 = hdng
+
+    new () =
+        let randomX = float (rnd.Next(0, w))
+        let randomY = float (rnd.Next(0, h))
         let randomHeading = rnd.NextDouble() * 360.0
         Moth((randomX, randomY), randomHeading)
-    member this.hdng = _hdng
-    member this.pos = _pos
-    member this.draw (): PrimitiveTree =
-      let (x,y) = _pos
-      filledEllipse lightGray 5 5 
-      |> translate x y
-    member this.move (lightOn: bool) = 
-        _hdng <- _hdng + (rnd.NextDouble() - 0.5) * 10.0
-        // Konverter retning til vektorer
-        let dx = Math.Cos(_hdng * Math.PI / 180.0) * 2.0
-        let dy = Math.Sin(_hdng * Math.PI / 180.0) * 2.0
-        // Hold møllen inden for lærredet
-        _pos <- (donutCoordinate ((_pos |> fst) + dx) (float width),
-                donutCoordinate ((_pos |> snd) + dy) (float height))
-    
 
-//Function generating a list of moths
-let generateMoths amount : Moth list =
+    member this.pos = pos2
+    member this.hdng = hdng2
+
+    member this.draw(): PrimitiveTree =
+        let (x, y) = pos2
+        filledEllipse lightGray 5 5 
+        |> translate x y
+
+    member this.move(lightOn: bool) =
+        if lightOn then
+            // Calculate vector to light
+            let (lx, ly) = lightPos
+            let (x, y) = pos2
+            let dx = lx - x
+            let dy = ly - y
+            let dist = Math.Sqrt(dx * dx + dy * dy)
+            // Normalize and move towards light
+            let normDx = dx / dist
+            let normDy = dy / dist
+            pos2 <- (coordinates (x + normDx * 2.0) (float w),
+                    coordinates (y + normDy * 2.0) (float h))
+        else
+            // Random wandering when light is off
+            hdng2 <- hdng2 + (rnd.NextDouble() - 0.5) * 10.0
+            let dx = Math.Cos(hdng2 * Math.PI / 180.0) * 2.0
+            let dy = Math.Sin(hdng2 * Math.PI / 180.0) * 2.0
+            let (x, y) = pos2
+            pos2 <- (coordinates (x + dx) (float w),
+                    coordinates (y + dy) (float h))
+
+
+// Function generating a list of moths
+let makeMoths amount: Moth list =
     [ for i in 1..amount -> Moth() ]
-let moths = generateMoths 5
 
-//Function easing the usage of the onto function - taken from miniGame.fsx on DIKU-Canvas GitHub
+let moths = makeMoths 100
+
+// Function easing the usage of the onto function - taken from miniGame.fsx on DIKU-Canvas GitHub
 let (++) p1 p2 = onto p1 p2
 
-//Function that separates the moths in the Moth list and draws them via the this.draw member
+// Function that separates the moths in the Moth list and draws them via the this.draw member
 let drawMoths (moths: Moth list) =
     moths
     |> List.fold (fun acc moth -> acc ++ (moth.draw())) emptyTree
 
-//function that makes the moths move
+// Function that makes the moths move
 let moveMoths (moths: Moth list) (lightOn: bool) =
     moths |> List.iter (fun moth -> moth.move(lightOn))
 
-
-//Light design elements
-let lampCoord =
-    filledRectangle darkGrey 2.0 (float height / 3.0)
-    |> translate (float width / 2.0) 0.0
-let lampShade = 
-    filledPolygon darkGray [(-80.0,40.0);(-40.0,0.0);(40.0,0.0);(80.0, 40.0)]
-    |> translate (float width / 2.0) (float height / 3.0)
-let lightBulbOff = 
-    filledEllipse whiteSmoke 25.0 25.0
-    |> translate (float width / 2.0) ((float height / 3.0)+40.0)
+// Light design elements
 let lightBulbOn =
-    filledEllipse yellow 25.0 25.0
-    |> translate (float width / 2.0) ((float height / 3.0)+40.0)
-let lightEffect =
-    let effectColor = fromRgba 235 186 52 100
-    filledEllipse effectColor 40.0 40.0
-    |> translate (float width / 2.0) ((float height / 3.0)+40.0)
-let uplitWall = 
-    let litUp = filledRectangle (fromRgba 254 207 2 160) width height
-    let shadows = filledPolygon (fromRgb 33 27 22) [(-(float width / 2.0),-float height/3.0);(-float width / 2.0,float height * 2.0/3.0);(-80.0,40.0);(80.0,40.0);(float width/2.0, float height * 2.0/3.0);((float width / 2.0),-float height/3.0)] 
-                    |> translate (float width / 2.0) (float height / 3.0)
-    shadows ++ litUp
+    filledEllipse white 25.0 25.0
+    |> translate (fst lightPos) (snd lightPos)
 
-//Function that draws the light and turns it on/off
+// Function that draws the light and turns it on/off
 let drawLight (state: bool) = 
-  if state then
-      lampCoord ++ lampShade ++ lightEffect ++ lightBulbOn ++ uplitWall 
-  else 
-    //   lampCoord ++ lampShade ++ lightBulbOff
-    emptyTree
+    if state then
+        lightBulbOn
+    else 
+        emptyTree
 
-//Draw function that collects the different object drawn above into one
+// Draw function that collects the different objects drawn above into one
 let draw (state: bool) =
     (drawMoths moths) ++ (drawLight state) |> make
 
-//React function, considers intercations in the canvas
-let react (state: bool) (ev: Event) : bool option =
-    match ev with
+// React function, considers interactions in the canvas
+let react (state: bool) (event: Event): bool option =
+    match event with
     | Event.TimerTick -> 
-        moveMoths moths state // Opdater møllernes position ved hvert tick
-        Some state
+        moveMoths moths state // Update moths' positions at each tick
+        Some state 
     | Event.Key k ->
         match Key k with
         | Key ' ' -> // Handle space as the toggle for the light
@@ -115,10 +114,8 @@ let react (state: bool) (ev: Event) : bool option =
         | _ -> Some state
     | _ -> Some state
 
+// Simulation, the unknown function makes sure the draw function takes the initialState and react into consideration
+interact "Moths" w h (Some 30) (fun state -> draw state) react initialState
 
-
-
-//Simulation, the unknown function makes sure, the draw function takes the initialState and react into consideration
-interact "Moths" width height (Some 30) (fun state -> draw state) react initialState
 
 
